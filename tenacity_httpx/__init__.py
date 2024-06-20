00e676717bf0dabe7cc399ba5e336df1b9bcaf51
Austin de Coup-Crank <austindcc@gmail.com>
2024-06-20 13:37:17 -0500

    Change default retry tuples to sets

--- tenacity_httpx/__init__.py
+++ tenacity_httpx/__init__.py
@@ -8,21 +8,21 @@ from tenacity import (retry_any, retry_if_exception, retry_if_exception_type,
 MAX_ATTEMPTS = 3
 
 # Default potentially transient HTTP error statuses to retry.
-RETRY_HTTP_STATUSES = (
+RETRY_HTTP_STATUSES = {
     httpx.codes.TOO_MANY_REQUESTS,
     httpx.codes.INTERNAL_SERVER_ERROR,
     httpx.codes.BAD_GATEWAY,
     httpx.codes.GATEWAY_TIMEOUT,
     httpx.codes.SERVICE_UNAVAILABLE,
-)
+}
 
 # Default potentially transient network errors to retry.
-RETRY_NETWORK_ERRORS = (
+RETRY_NETWORK_ERRORS = {
     httpx.ConnectError,
     httpx.ConnectTimeout,
     httpx.ReadError,
     httpx.WriteError,
-)
+}
 
 
 def _retry_http_errors(exc: Exception) -> bool:
