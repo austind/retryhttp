569ebe1f09a10e165d93319254b163cc71791db9
Austin de Coup-Crank <austindcc@gmail.com>
2024-06-22 11:41:57 -0500

    Remove unused import

--- tests/test_tenacity_httpx.py
+++ tests/test_tenacity_httpx.py
@@ -1,9 +1,7 @@
 import httpx
-from tenacity_httpx import retry_http_errors
 
 
 def test_retry_http_errors(respx_mock):
     respx_mock.get("https://example.com/").mock(return_value=httpx.Response(200))
-
     response = httpx.get("https://example.com/")
     assert response.status_code == 200
