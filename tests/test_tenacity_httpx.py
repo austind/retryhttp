b6c312f943dd9f88a598227bc66cfd77553c0f63
Austin de Coup-Crank <austindcc@gmail.com>
2024-06-22 11:41:31 -0500

    Scaffolding for tests

---
+++ tests/test_tenacity_httpx.py
@@ -0,0 +1,9 @@
+import httpx
+from tenacity_httpx import retry_http_errors
+
+
+def test_retry_http_errors(respx_mock):
+    respx_mock.get("https://example.com/").mock(return_value=httpx.Response(200))
+
+    response = httpx.get("https://example.com/")
+    assert response.status_code == 200
