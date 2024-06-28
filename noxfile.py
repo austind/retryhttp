7cc8e6efd64c3e31287ebc91250d9b8c29d18ca5
Austin de Coup-Crank <austindcc@gmail.com>
2024-06-28 06:31:40 -0500

    Tag tests

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,7 @@
 import nox
 
 
-@nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12"])
-def tests(session: nox.Session) -> None:
+@nox.session(python=["3.8", "3.9", "3.10", "3.11", "3.12"], tags=["test"])
+def test(session: nox.Session) -> None:
     session.install(".", ".[dev]")
     session.run("pytest", "-n", "4")
