f5fcebb0ab5a53727a0892aa2eb213ee3bf450d1
Austin de Coup-Crank <austindcc@gmail.com>
2024-08-13 11:57:02 -0500

    Work on tests

--- tests/test_rate_limited.py
+++ tests/test_rate_limited.py
@@ -48,30 +48,41 @@ def test_rate_limited_failure():
 
 @respx.mock
 def test_rate_limited_failure_httpdate():
+    http_date_2sec = get_http_date(delta_seconds=2)
+    http_date_3sec = get_http_date(delta_seconds=3)
+    http_date_4sec = get_http_date(delta_seconds=4)
     route = respx.get(MOCK_URL).mock(
         side_effect=[
-            rate_limited_response(retry_after=get_http_date(delta_seconds=2)),
-            rate_limited_response(retry_after=get_http_date(delta_seconds=2)),
-            rate_limited_response(retry_after=get_http_date(delta_seconds=2)),
+            rate_limited_response(retry_after=http_date_2sec),
+            rate_limited_response(retry_after=http_date_3sec),
+            rate_limited_response(retry_after=http_date_4sec),
         ]
     )
     with pytest.raises(RetryError):
         retry_rate_limited()
     assert route.call_count == 3
+    assert route.calls[0].response.status_code == 429
+    assert route.calls[0].response.headers.get("retry-after") == http_date_2sec
+    assert route.calls[1].response.status_code == 429
+    assert route.calls[1].response.headers.get("retry-after") == http_date_3sec
     assert route.calls[2].response.status_code == 429
+    assert route.calls[2].response.headers.get("retry-after") == http_date_4sec
 
 
 @respx.mock
 def test_rate_limited_success():
+    http_date = get_http_date(delta_seconds=2)
     route = respx.get(MOCK_URL).mock(
         side_effect=[
-            rate_limited_response(),
-            rate_limited_response(),
+            rate_limited_response(retry_after=1),
+            rate_limited_response(retry_after=http_date),
             httpx.Response(200),
         ]
     )
     response = retry_rate_limited()
     assert route.calls[0].response.status_code == 429
     assert route.calls[1].response.status_code == 429
+    assert route.calls[1].response.headers.get("retry-after") == http_date
     assert response.status_code == 200
+    assert response.headers.get("retry-after") is None
     assert route.call_count == 3
